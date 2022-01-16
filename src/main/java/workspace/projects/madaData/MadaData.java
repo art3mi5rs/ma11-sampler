da60bb33883296bc50dfa729544e584a745d9226
Shachaf <shachaf.smith@gmail.com>
2022-01-16 17:59:46 +0200

    created MadaData skeleton

--- src/main/java/workspace/projects/madaData/MadaData.java
+++ src/main/java/workspace/projects/madaData/MadaData.java
@@ -1,5 +1,27 @@
 package workspace.projects.madaData;
 
+import workspace.projects.madaData.Parsing.FileParser;
+import workspace.projects.madaData.Transforming.DataTransformer;
+import workspace.projects.madaData.Translating.DataTranslator;
+
 public class MadaData {
-    
+    FileParser parser;
+    DataTransformer transformer;
+    DataTranslator translator;
+
+    //Allows for data transformation
+    public MadaData(FileParser parser, DataTransformer transformer, DataTranslator translator) {
+        this.parser = parser;
+        this.transformer = transformer;
+        this.translator = translator;
+    }
+
+    public MadaData(FileParser parser, DataTranslator translator) {
+        this.parser = parser;
+        this.translator = translator;
+    }
+
+    public void runProgram() {
+        //write method code here
+    }
 }
--- src/main/java/workspace/projects/madaData/Main.java
+++ src/main/java/workspace/projects/madaData/Main.java
@@ -1,8 +1,10 @@
 package workspace.projects.madaData;
 
 public class Main {
-    public static void main(String args[]){
-        MadaData data = new MadaData();
+    public static void main(String args[]) {
+        //MadaData data = new MadaData(add params);
+        //data.runProgram()
+
         //write main method code here
     }
 }
