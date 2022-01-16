be93119b4bbf7ac4b224b18ce7667bd298972f89
Shachaf <shachaf.smith@gmail.com>
2022-01-16 17:41:49 +0200

    created Transformer Skeleton

--- src/main/java/workspace/projects/madaData/Transforming/DataTransformer.java
+++ src/main/java/workspace/projects/madaData/Transforming/DataTransformer.java
@@ -1,4 +1,18 @@
 package workspace.projects.madaData.Transforming;
 
-public class DataTransformer {
+import workspace.projects.madaData.People.Person;
+
+import java.util.HashSet;
+
+public abstract class DataTransformer {
+
+    /**
+     * Transforms the data to fit the requirements
+     * note: additional subclasses of Person might need to be created to match data
+     *
+     * @param people An array of Person instances
+     * @return Returns a transformed array of Person instances
+     */
+    public abstract HashSet<Person> transform(HashSet<Person> people);
+    
 }
